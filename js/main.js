e9acd08b93bc9d8a890540afc99a4dcba41274e2
Michael Atkinson <mikeatk88@gmail.com>
2016-06-03 14:36:03 +0900

    add successful API call

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+/*
+Look up and load new quotes.
 
+ - Lookup new random quotes using forismatic API found here:
+ 	http://forismatic.com/en/api/	
+ - If API call fails, get a random quote from a small local database
+ - Load quote into the page using jquery
+*/
+
+
+function quoteFromAPI () {
+	var APIendpoint = 'http://api.forismatic.com/api/1.0/?jsonp=?'
+	var options = {
+		lang : "en",
+		method : "getQuote",
+		format : "jsonp",
+	};
+	function callback (data, status, jrXHR) {
+		console.log("The status is " + status);
+		console.log("The quote is: " + data.quoteText);
+	}
+
+	$.getJSON(APIendpoint, options, callback);
+}
+
+quoteFromAPI();
